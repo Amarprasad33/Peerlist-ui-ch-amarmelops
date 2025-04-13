e7151decd303cf416f348961356861d35f299a84
amar <amarp4578@gmail.com>
2025-04-13 14:33:38 +0530

    added motion

--- src/components/day2.tsx
+++ src/components/day2.tsx
@@ -48,7 +48,7 @@ export default function FluidState() {
   }, []);
 
   const jerkAnimation = {
-    initial: { x: 0 },
+    initial: { x: -20 },
     animate: {
       x: [0, -5, 5, -4, 4, -2, 2, 0], // quick shakes
       transition: { duration: 0.6, ease: 'easeInOut', delay: 0.4 }
@@ -66,13 +66,18 @@ export default function FluidState() {
                 <AnimatePresence mode="wait">
                     <motion.div className='flex gap-1'>
                         {/* {icons[status]} */}
-                        {status === STATUS.ANALYZING && <div role="status" className='z-20'>
+                        {status === STATUS.ANALYZING && <motion.div 
+                            role="status" className='z-20'
+                            initial={{x: -20}}
+                            animate={{x: 0}}
+                            transition={{ duration: 0.4 }}
+                        >
                             <svg aria-hidden="true" className="inline w-4 h-4 text-gray-200 animate-spin dark:text-[#c5e4ff] fill-[#54b2ff]" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                             </svg>
                             <span className="sr-only">Loading...</span>
-                        </div>}
+                        </motion.div>}
                         {status === STATUS.WARNING && <motion.div
                                 variants={jerkAnimation}
                                 initial="initial"
@@ -84,7 +89,12 @@ export default function FluidState() {
                                 </svg>
                             </motion.div>
                         }
-                        {status === STATUS.SAFE && <svg className='mt-[3px]' width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        {status === STATUS.SAFE && <motion.svg 
+                            className='mt-[3px]' width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"
+                            initial={{x: -10}}
+                            animate={{x: 0}}
+                            transition={{ duration: 0.4 }}
+                        >
                             <g clipPath="url(#clip0_28_4)">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M0 7.5C0 5.51088 0.790176 3.60322 2.1967 2.1967C3.60322 0.790176 5.51088 0 7.5 0C9.48912 0 11.3968 0.790176 12.8033 2.1967C14.2098 3.60322 15 5.51088 15 7.5C15 9.48912 14.2098 11.3968 12.8033 12.8033C11.3968 14.2098 9.48912 15 7.5 15C5.51088 15 3.60322 14.2098 2.1967 12.8033C0.790176 11.3968 0 9.48912 0 7.5ZM7.072 10.71L11.39 5.312L10.61 4.688L6.928 9.289L4.32 7.116L3.68 7.884L7.072 10.71Z" fill="#34C759"/>
                             </g>
@@ -93,7 +103,7 @@ export default function FluidState() {
                                     <rect width="15" height="15" fill="white"/>
                                     </clipPath>
                                 </defs>
-                            </svg>
+                            </motion.svg>
                         }
                         <motion.div
                             key={status}
